9b4cbc7659a07e4c72ce02f85553112784942ddb
Saarthak <saarthakgupta08@gmail.com>
2019-08-08 15:26:05 +0530

    Added String Interpolation

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,13 @@
 import 'package:SampleDart/SampleDart.dart' as SampleDart;
 
-main(List<String> arguments) {
+void main(List<String> arguments) {
   print('Hello world: ${SampleDart.calculate()}!');
+
+  var myString=myName();
+  print('Welcome \n${myString}\nLength:${myString.length}');  //${myString} is String Interpolation
+}
+
+
+String myName(){
+  return 'Hey! I am Saarthak';
 }
