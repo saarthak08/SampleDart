a97203c532f222b8af2a71df8eef1af2319d8502
Saarthak <saarthakgupta08@gmail.com>
2019-08-13 12:35:27 +0530

    Added JSON parsing

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,11 @@
+import 'dart:collection';
+
 import 'package:SampleDart/Person.dart';
 import 'package:SampleDart/SampleDart.dart' as SampleDart;
 import 'package:SampleDart/Slot.dart';
 import 'package:SampleDart/circle.dart';
 import 'package:SampleDart/square.dart';
+import 'dart:convert';
 
 void main(List<String> arguments) {
   print('Hello world: ${SampleDart.calculate()}!');
@@ -15,8 +18,26 @@ void main(List<String> arguments) {
   person.printName();
   var circleSlot=Slot<circle>();
   circleSlot.insert(circle());
+  var rawJSON='{"url": "http:blah.com", "id": 1}';
+  var parsedJSON=json.decode(rawJSON);   //Parsed JSON is a hashmap
+  //var imageModel=ImageModel(parsedJSON['url'],parsedJSON['id']);
+  var imageModel=ImageModel.fromJSON(parsedJSON);
+  print(imageModel);
+  print('The URL: '+imageModel.url);
 }
 
+
+class ImageModel{
+  String url;
+  int id;
+
+  ImageModel(this.url,this.id);
+
+  ImageModel.fromJSON(parsedJSON){
+    this.url=parsedJSON['url'];
+    this.id=parsedJSON['id'];
+  }
+}
 
 String myName(){
   return 'Saarthak';
