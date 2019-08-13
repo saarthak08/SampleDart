224956d4344d925d4a379d9ffb8f8c43a339a097
Saarthak <saarthakgupta08@gmail.com>
2019-08-14 00:30:11 +0530

    Added futures

--- bin/main.dart
+++ bin/main.dart
@@ -1,15 +1,16 @@
 import 'dart:collection';
 
 import 'package:SampleDart/Person.dart';
-import 'package:SampleDart/SampleDart.dart' as SampleDart;
+import 'package:SampleDart/SampleDart.dart' as SampleDart;  //as is used for assigning the namespace to a library
 import 'package:SampleDart/Slot.dart';
 import 'package:SampleDart/circle.dart';
 import 'package:SampleDart/square.dart';
 import 'dart:convert';
+import 'dart:async';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
+
   print('Hello world: ${SampleDart.calculate()}!');
-
   var myString=myName();
   print('Welcome \n${myString}\nLength:${myString.length}');  //${myString} is String Interpolation
   var person=Person(myName());
@@ -24,6 +25,23 @@ void main(List<String> arguments) {
   var imageModel=ImageModel.fromJSON(parsedJSON);
   print(imageModel);
   print('The URL: '+imageModel.url);
+
+  print('About to fetch the data');
+  get("http://www.google.com").then((result){
+      print(result);
+  });
+
+  var result = await get('http://google.com');
+  print ('This is the awaited result:' +result);
+
+}
+
+
+Future<String> get(String url){
+  return Future.delayed(new Duration(seconds:3),()
+  {
+      return 'Got the data';
+  });
 }
 
 
